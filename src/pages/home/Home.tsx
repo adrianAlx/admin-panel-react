8bbaba09812104dce725b66f8d4ad3822d299c98
Adrian Changalombo <adrian.changalombo45@gmail.com>
2022-06-22 10:09:50 -0500

    [ADD]: TableList Componen added -> Latest transactions

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import { Chart } from '../../components/chart/Chart';
 import { Featured } from '../../components/featured/Featured';
+import { TableList } from '../../components/table/TableList';
 import { Widget } from '../../components/widget/Widget';
 
 import './home.scss';
@@ -17,6 +18,12 @@ export const Home = () => {
       <div className="charts">
         <Featured />
         <Chart title="Last 6 Months (Revenue)" aspect={3 / 1} />
+      </div>
+
+      {/* MUI */}
+      <div className="listContainer">
+        <div className="listTitle">Latest transactions</div>
+        <TableList />
       </div>
     </div>
   );
