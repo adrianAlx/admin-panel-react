5e34dba4619870b3aba0d258b2746ad0c8693306
Adrian Changalombo <adrian.changalombo45@gmail.com>
2022-06-22 22:02:10 -0500

    [ADD]: single page added

--- src/pages/list/List.tsx
+++ src/pages/list/List.tsx
@@ -1,12 +1,9 @@
 import { Datatable } from '../../components/datatable/Datatable';
-import './list.scss';
 
 export const List = () => {
   return (
-    <div className="list">
-      <div className="listContainer">
-        <Datatable />
-      </div>
+    <div className="listContainer">
+      <Datatable />
     </div>
   );
 };
